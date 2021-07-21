bedc398a3454ff9e2d449c4347ace90f5d0a5e5d
Harish <harishsidagam@gmail.com>
2021-07-21 06:00:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+
+
+link = ['https://register.sidagamhareesh.live','https://youtube.com','https://facebook.com','https://twitter.com','https://web.whatsapp.com/']
+
+for i in link:
+    webbrowser.open(i)
+
+
